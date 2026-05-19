d7aada7331d21478997d0d7e78c2403bb8bffb1d
Asanaly Duzkenov <asan@MacBook-Pro-Aliya.local>
2026-05-20 00:30:51 +0800

    Switch gateway proxy targets to public URLs for auto-start

--- svc-gateway/cmd/main.go
+++ svc-gateway/cmd/main.go
@@ -30,8 +30,8 @@ func main() {
 		slog.String("port", cfg.Port),
 	)
 
-	userTarget, _ := url.Parse("http://videoforge-user.flycast:8080")
-	briefTarget, _ := url.Parse("http://videoforge-brief.flycast:8080")
+	userTarget, _ := url.Parse("https://videoforge-user.fly.dev")
+	briefTarget, _ := url.Parse("https://videoforge-brief.fly.dev")
 
 	userProxy := httputil.NewSingleHostReverseProxy(userTarget)
 	briefProxy := httputil.NewSingleHostReverseProxy(briefTarget)
